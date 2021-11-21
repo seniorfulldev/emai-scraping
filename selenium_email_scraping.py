3ae40916d275af2784232fae1fe3b7e745b2375d
seniorfulldev <piaoyilong114@gmail.com>
2021-11-22 04:27:18 +0800

    add mutilthreading

--- selenium_email_scraping.py
+++ selenium_email_scraping.py
@@ -1,9 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get('http://www.digitalseo.in/')
+r = requests.get('http://www.freelancer.com/', 'lxml')
+# r = BeautifulSoup(requests.get('http://www.digitalseo.in/').text, 'lxml')
 data = r.text
-soup = BeautifulSoup(data)
+soup = BeautifulSoup(data, 'lxml')
 
 for rate in soup.find_all('@'):
     print(rate.text)
